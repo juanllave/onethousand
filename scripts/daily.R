bad5c543d7f783044fe0b6f4b91d629ab5e5038b
Lozano Vallejo <JLOZANOVALLEJO1@calgary.ca>
2026-03-24 10:56:43 -0600

    Mid morning update.

--- scripts/daily.R
+++ scripts/daily.R
@@ -14,8 +14,8 @@ next_album
 next_album <- next_album %>% 
   replace_na(list(
     # Replace with correct values after each album has been listened to
-    Rating = 4, 
-    Notes = 'They did everything right, every single time.',
+    Rating = 3, 
+    Notes = 'Fun. Almost there.',
     Origin = 'us',
     `Generated Date` = Sys.Date()
   ))
