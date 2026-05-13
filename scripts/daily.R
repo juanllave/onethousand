9ce7e563c00a56ebfceb477e2e0cfc6657f57913
Lozano Vallejo <JLOZANOVALLEJO1@calgary.ca>
2026-05-13 12:59:10 -0600

    May 13. AM.

--- scripts/daily.R
+++ scripts/daily.R
@@ -14,9 +14,9 @@ next_album
 next_album <- next_album %>% 
   replace_na(list(
     # Replace with correct values after each album has been listened to
-    Rating = 4, 
-    Notes = 'A total tool. A great musician.',
-    Origin = 'england',
+    Rating = 3, 
+    Notes = 'Meh.',
+    Origin = 'canada',
     `Generated Date` = Sys.Date()
   ))
 
